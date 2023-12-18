8e0b01b8460fa6276f0d75f126f8012ccb9ba9dc
Choco02 <aliceassis455@gmail.com>
2023-12-18 19:22:31 -0300

    feat: add profile page

---
+++ src/components/ProjectsGrid.tsx
@@ -0,0 +1,48 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import useMediaQuery from "@mui/material/useMediaQuery"
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { grey } from "@mui/material/colors";
+
+interface ProjectData {
+  id: string;
+  title: string;
+  description: string;
+  url: string;
+}
+
+export default function ProjectsGrid({
+  projectsData,
+}: {
+  projectsData: ProjectData[] | [];
+}) {
+    const isDesktop = useMediaQuery('(min-width:600px)');
+
+  return (
+    <Box display="flex" flexWrap="wrap" sx={{ width: "70vw"}} >
+    <Grid container spacing={2}>
+      {projectsData.map((projectData) => (
+        <Grid item xs={isDesktop ? 3 : 6}>
+          <Card key={projectData.id} sx={{ maxWidth: 500, bgcolor: grey[800] }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color={grey[100]} gutterBottom>
+                {projectData.title}
+              </Typography>
+              <Typography color={grey[100]}>
+                {projectData.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Detalhes</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+    </Box>
+  );
+}
---
+++ src/pages/Profile.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+import Container from "@mui/material/Container";
+import Header from "../components/Header";
+import ProjectsGrid from "../components/ProjectsGrid";
+
+export default function Profile() {
+
+  const [projectsData, /** setProjectsData*/] = useState([
+    {
+      id: '1',
+      title: 'titulo 1',
+      description: 'descricao 1',
+      url: 'url1'
+    },
+    {
+      id: '2',
+      title: 'titulo 2',
+      description: 'descricao 2',
+      url: 'url2'
+    },
+    {
+      id: '3',
+      title: 'titulo 3',
+      description: 'descricao 3',
+      url: 'url3'
+    },
+    {
+      id: '4',
+      title: 'titulo 4',
+      description: 'descricao 4',
+      url: 'url4'
+    },
+    {
+      id: '5',
+      title: 'titulo 5',
+      description: 'descricao 5',
+      url: 'url5'
+    }
+  ])
+
+  return (
+    <Container maxWidth="md" >
+      <Header />
+      <ProjectsGrid projectsData={projectsData} />
+    </Container>
+  );
+}
